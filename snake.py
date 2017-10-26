ffd4a5c77ef727c64d5fb419ef5fe0886c3c792d
Jatin <jatinj615@gmail.com>
2017-10-26 12:59:32 +0530

    centring text

--- snake.py
+++ snake.py
@@ -27,10 +27,16 @@ def snake(block_size, snakeList):
 	for XnY in snakeList:
 		pygame.draw.rect(gameDisplay, green, [XnY[0],XnY[1],block_size,block_size])
 	
+def text_objects(text, color):
+	textSurface = font.render(text, True, color)
+	return textSurface, textSurface.get_rect()
 
 def message_to_screen(msg, color):
-	screen_text = font.render(msg, True, color)
-	gameDisplay.blit(screen_text, [display_width/2, display_height/2])
+	textSurf, textRect = text_objects(msg, color)
+	# screen_text = font.render(msg, True, color)
+	# gameDisplay.blit(screen_text, [display_width/2, display_height/2])
+	textRect.center = (display_width /2), (display_height/2)
+	gameDisplay.blit(textSurf, textRect)
 
 def gameLoop():
 	gameExit = False
@@ -54,6 +60,9 @@ def gameLoop():
 			pygame.display.update()
 
 			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					gameExit = True
+					gameOver = False
 				if event.type == pygame.KEYDOWN:
 					if event.key == pygame.K_q:
 						gameExit = True
